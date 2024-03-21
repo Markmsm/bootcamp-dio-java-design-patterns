32138164ec1caf8edc5a6d1ba228045ec76f6753
Mark <marksmunari@gmail.com>
2024-03-21 12:20:29 -0300

    add repository injection in CustomerServiceImpl

--- src/main/java/com/designpatterns/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/designpatterns/service/impl/CustomerServiceImpl.java
@@ -3,8 +3,10 @@ package com.designpatterns.service.impl;
 import com.designpatterns.model.Customer;
 import com.designpatterns.repository.CustomerRepository;
 import com.designpatterns.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class CustomerServiceImpl implements CustomerService {
+    @Autowired
     CustomerRepository repository;
     @Override
     public Iterable<Customer> getAll() {
